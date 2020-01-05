fbf31bbc03f20c03c41f678362d1f5e9ae071816
truble <truble@google.com>
2020-01-05 21:25:29 +0000

    Added dm.sh

---
+++ dm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gcloud deployment-manager deployments describe xom-app-folders > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+   COMMAND=update
+else
+   COMMAND=create
+fi
+
+gcloud deployment-manager deployments $COMMAND xom-app-folders --config xom-app-folders.yaml
