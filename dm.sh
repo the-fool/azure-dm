07dd8183926f4a633b8153b09117a37a6358138d
truble <truble@google.com>
2020-01-05 17:01:10 -0600

    env

--- dm.sh
+++ dm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $DM_PROJECT
+env
 gcloud config set project premium-modem-261922
 gcloud deployment-manager deployments describe xom-app-folders > /dev/null 2>&1
 if [ $? -eq 0 ]; then
