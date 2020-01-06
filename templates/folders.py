51a16e047e389426b6a7242e09e14434f5f7be28
Thomas Ruble <sketchbang@gmail.com>
2020-01-06 13:18:00 -0600

    Update folders.py

--- templates/folders.py
+++ templates/folders.py
@@ -1,3 +1,5 @@
+# Copyright 2019 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose. Your use of it is subject to your agreement with Google.  
+
 def generate_config(ctx):
     props = ctx.properties
     root_node = ctx.env['name']
