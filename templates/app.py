a84901a6da0f043214f35c0a01f5fa8a8d4c5c3f
Thomas Ruble <sketchbang@gmail.com>
2020-01-06 13:17:38 -0600

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -1,3 +1,5 @@
+# Copyright 2019 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose. Your use of it is subject to your agreement with Google.  
+
 # Common naming definitions
 def get_billing_account_name(project_id):
     return "{}-billing".format(project_id)
